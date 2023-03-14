0fbab8b46f4b77303083dd3f166e1b7582425086
Aditya goswami <giriaditya26@gmail.com>
2023-03-14 17:01:49 +0530

    Change in main class

--- G5-B5_FOP_GroupAssignment1/src/com/assignment/main/Main.java
+++ G5-B5_FOP_GroupAssignment1/src/com/assignment/main/Main.java
@@ -12,23 +12,23 @@ public class Main {
         AdminDepartment admin = new AdminDepartment();
 
         // Invoking or calling methods of Admin department
-        System.out.println("Welcome to " + admin.departmentName());
+        System.out.println(" Welcome to " + admin.departmentName());
         System.out.println(admin.getTodaysWork());
         System.out.println(admin.getWorkDeadline());
         System.out.println(admin.isTodayAHoliday() + '\n');
 
         // Invoking or calling methods of HR department
-        System.out.println("Welcome to " + hr.departmentName());
+        System.out.println(" Welcome to " + hr.departmentName());
         System.out.println(hr.doActivity());
         System.out.println(hr.getTodaysWork());
         System.out.println(hr.getWorkDeadline());
         System.out.println(hr.isTodayAHoliday() + '\n');
 
         // Invoking or calling methods of Tech department
-        System.out.println("Welcome to " + tech.departmentName());
+        System.out.println(" Welcome to " + tech.departmentName());
         System.out.println(tech.getTodaysWork());
         System.out.println(tech.getWorkDeadline());
-        System.out.println(tech.getTechStackInformation());
+        System.out.println(" " + tech.getTechStackInformation());
         System.out.println(tech.isTodayAHoliday());
     }
     // Creating objects for various departments
